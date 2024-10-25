1a416eb5b696106ea4b4919af73232a0e0845770
Sens <senswrong@gmail.com>
2024-10-25 16:07:37 +0800

    complete pc domain

--- common/js/main.js
+++ common/js/main.js
@@ -1,18 +1,3 @@
-var get_value = function (key) {//得到?后参数的确切值
-    var url = document.URL;
-    if (/\?/.test(url)) {
-        url = url.replace(/.*?\?/, '').replace(/\#.*/, '').split('&');
-        for (let x in url) {
-            if (/\=/.test(url[x])) {
-                let value = url[x].split('=');
-                if (value[0] == key) {
-                    return value[1];
-                    break;
-                }
-            }
-        }
-    }
-};
 $(function () {
     window.onload = () => {
         if (window.innerWidth < 1440)
