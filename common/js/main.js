bb052e3d0c6c4f0297c381b90d0f1e62e0472def
Sens <56918901+senswrong@users.noreply.github.com>
2026-04-26 11:58:53 +0800

    add web 2.0

--- common/js/main.js
+++ common/js/main.js
@@ -1,18 +1,17 @@
+//立即调用函数表达式
 $(function () {
-    window.onload = () => {
-        if (window.innerWidth < 1440)
-            document.documentElement.style.fontSize = window.innerWidth / 1440 + 'px';
-        else document.documentElement.style.fontSize = '1px';
+    let callback = () => {
+        //设置页面字体大小
+        document.documentElement.style.fontSize = window.innerWidth < 1440 ? window.innerWidth / 1440 + 'px' : '1px';
     }
-    window.onresize = () => {
-        if (window.innerWidth < 1440)
-            document.documentElement.style.fontSize = window.innerWidth / 1440 + 'px';
-        else document.documentElement.style.fontSize = '1px';
-    }
-    $('img').on('dragstart', function (event) {
-        event.preventDefault();
-    });
-    $('img').on('contextmenu', function (event) {
-        event.preventDefault();
-    });
+    //监听加载完成
+    window.onload = callback
+    //监听窗口大小变化
+    window.onresize = callback
+    //事件禁用
+    let cancel = event => event.preventDefault();
+    //禁止图片拖拽
+    $('img').on('dragstart', cancel);
+    //禁止图片右键
+    $('img').on('contextmenu', cancel);
 })
